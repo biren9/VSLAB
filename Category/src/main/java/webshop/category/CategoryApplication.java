a5d1c421da444bf5d7d164f49e601b0c89b5778f
nipfit00 <nico.pfaff1997@googlemail.com>
2020-12-08 08:43:45 +0100

    Category fixes
    Category setup quick fixes

--- Category/src/main/java/webshop/category/CategoryApplication.java
+++ Category/src/main/java/webshop/category/CategoryApplication.java
@@ -2,8 +2,11 @@ package webshop.category;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CategoryApplication {
 
 	public static void main(String[] args) {
