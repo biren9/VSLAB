6ad9c165e363e9963c8dfd9cd94ea2c07fcb93d5
nipfit00 <nico.pfaff1997@googlemail.com>
2020-12-08 13:09:20 +0100

    Catalog Setup
    set up catalog as composite service

---
+++ Catalog/src/main/java/webshop/catalog/CatalogApplication.java
@@ -0,0 +1,19 @@
+package webshop.catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+@SpringBootApplication
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@RibbonClient("catalog")
+public class CatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogApplication.class, args);
+	}
+
+}
