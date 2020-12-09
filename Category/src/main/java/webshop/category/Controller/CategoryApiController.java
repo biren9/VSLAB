65e02a100024b1244e18be866fbe88654940bc30
Gil Biren <gilbiren@gmail.com>
2020-12-09 16:05:31 +0100

    Implement category api

--- Category/src/main/java/webshop/category/Controller/CategoryApiController.java
+++ Category/src/main/java/webshop/category/Controller/CategoryApiController.java
@@ -1,10 +1,17 @@
 package webshop.category.Controller;
 
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import webshop.category.Model.Category;
 import webshop.category.Repository.CategoryRepository;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
 
 @RestController()
 public class CategoryApiController {
@@ -13,23 +20,44 @@ public class CategoryApiController {
     CategoryRepository categoryRepository;
 
     @GetMapping("/categories")
-    public String fetchCategories() {
-        return "OK";
+    public ResponseEntity<List<Category>> fetchCategories() {
+        List<Category> allCategories = categoryRepository.findAll();
+        return new ResponseEntity(allCategories, HttpStatus.OK);
     }
 
     @PostMapping("/categories")
-    public String updateCategory() {
-        return "OK";
+    public ResponseEntity<Boolean> createCategory(
+            @NotNull @ApiParam(value = "Category name", required = true) @Valid @RequestParam(value = "name", required = true) String name
+    ) {
+        Category newCategory = new Category(name);
+        categoryRepository.save(newCategory);
+        return new ResponseEntity(true, HttpStatus.OK);
     }
 
     @PutMapping("/categories/{id}")
-    public String createCategory(@PathVariable("id") Integer id) {
-        return "OK" + id;
+    public ResponseEntity<Boolean> updateCategory(
+            @PathVariable("id") Long id,
+            @NotNull @ApiParam(value = "Category name", required = true) @Valid @RequestParam(value = "name", required = true) String name
+    ) {
+        Optional<Category> optinalCategory = categoryRepository.findById(id);
+        if (optinalCategory.isPresent()) {
+            Category category = optinalCategory.get();
+            category.setName(name);
+            categoryRepository.save(category);
+            return new ResponseEntity(true, HttpStatus.OK);
+        }
+        return new ResponseEntity(false, HttpStatus.BAD_REQUEST);
     }
 
     @DeleteMapping("/categories/{id}")
-    public String deleteCategory(@PathVariable("id") Integer id) {
-        return "OK" + id;
+    public ResponseEntity deleteCategory(@PathVariable("id") Long id) {
+        Optional<Category> optinalCategory = categoryRepository.findById(id);
+        if (optinalCategory.isPresent()) {
+            Category category = optinalCategory.get();
+            categoryRepository.delete(category);
+            return new ResponseEntity(true, HttpStatus.OK);
+        }
+        return new ResponseEntity(false, HttpStatus.BAD_REQUEST);
     }
 
 
--- Category/src/main/java/webshop/category/Repository/CategoryRepository.java
+++ Category/src/main/java/webshop/category/Repository/CategoryRepository.java
@@ -2,5 +2,8 @@ package webshop.category.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import webshop.category.Model.Category;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Integer> {
+    Optional<Category> findById(Long id);
 }
