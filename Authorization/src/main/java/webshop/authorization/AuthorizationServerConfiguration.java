491c3a1ee696bddccbf8855b7689127be859249f
nipfit00 <nico.pfaff1997@googlemail.com>
2021-01-25 20:51:05 +0100

    removed exposed ports
    removed /product-service and /catalog-service as exposed ports
    minor fix in websecurityconfig

--- Authorization/src/main/java/webshop/authorization/AuthorizationServerConfiguration.java
+++ Authorization/src/main/java/webshop/authorization/AuthorizationServerConfiguration.java
@@ -1,7 +1,5 @@
 package webshop.authorization;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
